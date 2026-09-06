a8add57d60765591787152b5eaf9b05801e40225
daza06 <link123852@gmail.com>
2026-09-06 16:38:26 -0600

    Week 01 Project Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,3 @@
+document.getElementById("lastModified").textContent =
+  `Last Modification: ${document.lastModified}`;
+document.getElementById("currentyear").textContent = new Date().getFullYear();
